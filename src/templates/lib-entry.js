ae6534479d4bab00123bf5f1dbedd0818aaefed8
Rob MacKay <robskiwarrior@gmail.com>
2019-04-06 23:29:20 +0100

    quik test

--- src/templates/lib-entry.js
+++ src/templates/lib-entry.js
@@ -23,7 +23,7 @@ export const LibEntryTemplate = ({
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
+              {title} <span>here I am</span>
             </h1>
             <p>{description}</p>
             <PostContent content={content} />
